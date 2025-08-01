110f7cbacfb743d4a3781de0791b76c1823e7dd5
dramaticLemon <chebandima27@gmail.com>
2025-08-01 09:44:34 +0300

    Update common-join.sql

--- common-join.sql
+++ common-join.sql
@@ -34,3 +34,19 @@ FROM
     таблица_2 AS t2 -- Правая таблица
 ON
     t1.столбец = t2.столбец; -- Условие объединения
+
+
+
+
+SELECT
+    u.username,
+    p.name AS product_name,
+    oi.quantity
+FROM
+    shop.users AS u
+JOIN
+    shop.orders AS o ON u.id = o.user_id      -- join user with order
+JOIN
+    shop.order_items AS oi ON o.order_id = oi.order_id -- join orders with  products
+JOIN
+    shop.products AS p ON oi.products_id = p.product_id; -- join products with products info
